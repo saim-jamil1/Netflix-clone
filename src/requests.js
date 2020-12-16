08d69ac8c3f50157b71041a8a1b3215385fe965d
saim-jamil1 <saimjamil11@gmail.com>
2020-12-16 22:31:04 +0500

    First commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "1ca8c94b848ad1eb1de4e0ca34e3362a";
+
+const requests={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
